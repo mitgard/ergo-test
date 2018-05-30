bcbb64b813e3c58c87f5882a9e901809754e6962
mitgard <riksanov@innodata.ru>
2018-05-30 08:47:54 +0300

    first commit

--- src/main/scala/org/ergoplatform/BlockChainNodeActor.scala
+++ src/main/scala/org/ergoplatform/BlockChainNodeActor.scala
@@ -1,7 +1,7 @@
 package org.ergoplatform
 
 import akka.actor.{Actor, ActorRef, Props, Terminated}
-
+import akka.actor.Stash
 import scala.util.{Failure, Success}
 
 object BlockChainNodeActor {
@@ -13,14 +13,24 @@ object BlockChainNodeActor {
 
   case class ConnectTo(peer: ActorRef)
 
+  case class GetLastBlockAfter(block: Block)
+
+  case class LastBlock(block: Block, height: Int = 0)
 }
 
-class BlockChainNodeActor(startBlockChain: BlockChain, knownPeers: Seq[ActorRef]) extends Actor {
+class BlockChainNodeActor(startBlockChain: BlockChain, knownPeers: Seq[ActorRef]) extends Actor with Stash {
 
   import BlockChainNodeActor._
+  knownPeers.foreach(_ ! GetLastBlockAfter(startBlockChain.lastBlock.get))
 
-  override def preStart(): Unit =
+  override def preStart(): Unit ={
+    if(knownPeers.isEmpty)
     context.become(active(startBlockChain, knownPeers))
+    else {
+      knownPeers.foreach(_ ! ConnectTo(self))
+      context.become(receive)
+    }
+  }
 
   private def active(blockChain: BlockChain, peers: Seq[ActorRef]): Receive = {
     case GetConnectedPeers =>
@@ -29,7 +39,14 @@ class BlockChainNodeActor(startBlockChain: BlockChain, knownPeers: Seq[ActorRef]
       sender() ! blockChain
     case ConnectTo(node) =>
       context.watch(node)
-      context.become(active(blockChain, peers :+ node))
+      println(s" Self $self connect to $node")
+      if(!peers.contains(node)){
+        node ! ConnectTo(self)
+        node ! LastBlock(blockChain.lastBlock.get)
+        context.become(active(blockChain, peers :+ node))
+      }
+      else
+      context.become(active(blockChain, peers))
     case Terminated(terminatedNode) =>
       context.become(active(blockChain, peers.filter(_ != terminatedNode)))
     case b: Block => blockChain.append(b) match {
@@ -38,7 +55,35 @@ class BlockChainNodeActor(startBlockChain: BlockChain, knownPeers: Seq[ActorRef]
       case Failure(f) =>
         println(s"Error on apply block $b to blockchain $blockChain: $f")
     }
+    case getLastBlockAfter: GetLastBlockAfter =>
+      println(s"I'm $self send $sender() block: ${blockChain.filter(_.parentId.contains(getLastBlockAfter.block.id)).last}")
+      val lastBlockChain = blockChain.filter(_.parentId.contains(getLastBlockAfter.block.id)).last
+      sender() ! LastBlock(lastBlockChain)
+    case lastBlock: LastBlock =>
+      blockChain.append(lastBlock.block) match {
+        case Success(newBlockChain) =>
+          println(s"checked at ${System.currentTimeMillis()}   $self new BlockChain = $newBlockChain")
+          context.become(active(newBlockChain, knownPeers))
+        case Failure(f) =>
+          println(s"Error on synchronized block ${lastBlock.block} with error: $f")
+      }
   }
 
-  override def receive = Actor.emptyBehavior
+  override def receive: Receive = {
+    case lastBlock: LastBlock =>
+      startBlockChain.append(lastBlock.block) match {
+        case Success(newBlockChain) =>
+          println(s"checked at ${System.currentTimeMillis()}   $self new BlockChain = $newBlockChain")
+          unstashAll()
+          context.become(active(newBlockChain, knownPeers))
+        case Failure(f) =>
+          println(s"Error on synchronized block ${lastBlock.block} with error: $f")
+      }
+    case _ =>
+      if(knownPeers.isEmpty)
+        context.become(active(startBlockChain, knownPeers))
+      else
+      stash()
+  }
+
 }
